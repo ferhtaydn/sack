2ff828e27480a2ade85ffabc0e7651f2445237bc
Ferhat Aydin <ferhtaydn@gmail.com>
2016-10-25 21:12:33 +0300

    initial commit

---
+++ build.sbt
@@ -0,0 +1,52 @@
+import scalariform.formatter.preferences._
+import com.typesafe.sbt.SbtScalariform
+import com.typesafe.sbt.SbtScalariform.ScalariformKeys
+
+name := "sack"
+
+organization := "com.ferhtaydn"
+
+version := "0.1.0"
+
+scalaVersion := "2.11.8"
+
+crossScalaVersions := Seq("2.10.6", "2.11.8")
+
+resolvers ++= Seq(
+  "Sonatype OSS Snapshots" at "http://oss.sonatype.org/content/repositories/snapshots/"
+)
+
+libraryDependencies ++= Seq(
+  "net.cakesolutions" %% "scala-kafka-client" % "0.10.0.0",
+  "net.cakesolutions" %% "scala-kafka-client-akka" % "0.10.0.0",
+  "net.cakesolutions" %% "scala-kafka-client-testkit" % "0.10.0.0" % "test",
+  "org.scalatest" %% "scalatest" % "3.0.0" % "test",
+  "org.scalacheck" %% "scalacheck" % "1.13.3" % "test"
+)
+
+scalacOptions ++= Seq(
+    "-target:jvm-1.8",
+    "-deprecation",
+    "-encoding", "UTF-8",
+    "-feature",
+    "-language:existentials",
+    "-language:higherKinds",
+    "-language:implicitConversions",
+    "-language:experimental.macros",
+    "-unchecked",
+    "-Ywarn-unused-import",
+    "-Ywarn-nullary-unit",
+    "-Xfatal-warnings",
+    "-Xlint",
+    "-Yinline-warnings",
+    "-Ywarn-dead-code",
+    "-Xfuture")
+
+initialCommands := "import com.ferhtaydn.sack._"
+
+SbtScalariform.scalariformSettings
+
+ScalariformKeys.preferences := ScalariformKeys.preferences.value
+  .setPreference(AlignSingleLineCaseStatements, true)
+  .setPreference(DoubleIndentClassDeclaration, true)
+  .setPreference(RewriteArrowSymbols, true)
---
+++ src/main/scala/com/ferhtaydn/sack/RawProductConsumer.scala
@@ -0,0 +1,93 @@
+package com.ferhtaydn.sack
+
+import akka.actor.{ Actor, ActorLogging, ActorRef, ActorSystem, Props }
+import cakesolutions.kafka.akka.KafkaConsumerActor.{ Confirm, Subscribe }
+import cakesolutions.kafka.{ KafkaConsumer, KafkaProducer }
+import cakesolutions.kafka.akka._
+import com.typesafe.config.{ Config, ConfigFactory }
+import org.apache.kafka.common.serialization.{ StringDeserializer, StringSerializer }
+
+import scala.concurrent.duration._
+
+object RawProductConsumerBoot extends App {
+
+  val config = ConfigFactory.load()
+  val consumerConfig = config.getConfig("consumer")
+  val producerConfig = config.getConfig("producer")
+
+  RawProductConsumer(consumerConfig, producerConfig)
+
+}
+
+object RawProductConsumer {
+
+  def apply(consumerConfig: Config, producerConfig: Config): ActorRef = {
+
+    val consumerConf = KafkaConsumer.Conf(consumerConfig, new StringDeserializer, new StringDeserializer)
+
+    val actorConf = KafkaConsumerActor.Conf(1.seconds, 3.seconds)
+
+    val producerConf = KafkaProducer.Conf(producerConfig, new StringSerializer, new StringSerializer)
+
+    val system = ActorSystem("raw-product-consumer-system")
+
+    system.actorOf(
+      Props(new RawProductConsumer(consumerConf, actorConf, producerConf)),
+      "raw-product-consumer-actor"
+    )
+
+  }
+
+}
+
+class RawProductConsumer(
+    kafkaConsumerConf: KafkaConsumer.Conf[String, String],
+    consumerActorConf: KafkaConsumerActor.Conf,
+    kafkaProducerConf: KafkaProducer.Conf[String, String]
+) extends Actor with ActorLogging {
+
+  val recordsExt = ConsumerRecords.extractor[String, String]
+
+  val consumerActor = context.actorOf(
+    KafkaConsumerActor.props(kafkaConsumerConf, consumerActorConf, self),
+    "kafka-consumer-actor"
+  )
+
+  context.watch(consumerActor)
+
+  val producer = context.actorOf(
+    KafkaProducerActor.props(kafkaProducerConf),
+    "kafka-producer-actor"
+  )
+
+  consumerActor ! Subscribe.AutoPartition(List("product-csv-raw"))
+
+  override def receive: Receive = {
+
+    // Records from Kafka
+    case recordsExt(records) ⇒
+      processRecords(records)
+
+    // Confirmed Offsets from KafkaProducer
+    case o: Offsets ⇒
+      consumerActor ! Confirm(o, commit = true)
+  }
+
+  private def processRecords(records: ConsumerRecords[String, String]) = {
+
+    val transformedRecords = records.pairs.map {
+      case (key, value) ⇒
+        log.info(s"Received [$key, $value]")
+        (value, value.toUpperCase())
+    }
+
+    log.info(s"Batch complete, offsets: ${records.offsets}")
+
+    producer ! ProducerRecords.fromKeyValues[String, String](
+      "product-csv-raw-uppercase",
+      transformedRecords,
+      Some(records.offsets),
+      None
+    )
+  }
+}
